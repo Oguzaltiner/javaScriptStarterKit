f98447bf34a95b0d4434fcb208a25210c814db26
Oğuz Altıner <oguza244@gmail.com>
2021-10-19 00:01:58 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Kodlama.io");
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+console.log(dolarDun);
+
+const euroDun = 11;
+
+let konutKredileri = ["Konut ","Emlak Kredisi","Kamu Konut Kredisi"];
+console.log("<ul>");
+konutKredileri.forEach(element => {
+    console.log("<li>"+element+"</li>");
+});
+console.log("</ul>");
+
+
+console.log(konutKredileri);
